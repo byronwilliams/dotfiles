b2cc251c65ba999b2c447dfe07f47e62bb62a941
Byron Williams <byron@112percent.com>
2015-05-26 08:59:37 +0100

    Add Sakura

--- setup.sh
+++ setup.sh
@@ -5,3 +5,5 @@ ln -rs xinitrc ~/.xinitrc
 ln -rs xsession ~/.xsession
 ln -rs Xresources ~/.Xresources
 ln -rs config/fish/config.fish ~/.config/fish/config.fish
+ln -rs config/sakura/sakura.conf ~/.config/sakura/sakura.conf
+
