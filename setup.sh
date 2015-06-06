de77f7d72c4f2027baf22baca2d0d5aa3adc4725
Byron Williams <byron@112percent.com>
2015-06-06 22:09:54 +0100

    Add ST3 prefs

--- setup.sh
+++ setup.sh
@@ -6,5 +6,6 @@ ln -rs xsession ~/.xsession
 ln -rs Xresources ~/.Xresources
 ln -rs config/fish/config.fish ~/.config/fish/config.fish
 ln -rs config/sakura/sakura.conf ~/.config/sakura/sakura.conf
-ln -rs config/sakura/sakura.confredshift.conf ~/.config/redshift.conf
+ln -rs config/sakura/redshift.conf ~/.config/redshift.conf
 
+ln -rs config/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
