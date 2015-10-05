74ce36241085ae5081b85b3f80f7fd6b56f18fa5
Byron Williams <byron@swooneditions.com>
2015-10-05 07:28:05 +0100

    Add Atom config

--- setup.sh
+++ setup.sh
@@ -6,5 +6,9 @@ ln -rs xsession ~/.xsession
 ln -rs Xresources ~/.Xresources
 ln -rs config/fish/config.fish ~/.config/fish/config.fish
 ln -rs config/sakura/sakura.conf ~/.config/sakura/sakura.conf
-ln -rs config/sakura/sakura.confredshift.conf ~/.config/redshift.conf
+ln -rs config/sakura/sakura.conf
+ln -rs redshift.conf ~/.config/redshift.conf
 
+mkdir -p ~/.atom
+ls -rs atom/keymap.cson ~/.atom/keymap.cson
+ln -rs atom/config.cson ~/.atom/config.cson
